5d04c1ec029a9ec4f7f1928c5d72d4c23fd16b9c
qingyliu <qingyliu@microsoft.com>
2024-06-04 16:07:49 +0800

    add gradle e2e test

---
+++ gradle/src/main/java/io/paketo/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package io.paketo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(proxyBeanMethods=false)
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/check")
+    public String check() {
+        return "Hello World!";
+    }
