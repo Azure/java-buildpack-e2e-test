e1bb134ea648f34fbcaf2739b0646417daa7db05
Qingyi Liu <34502364+smile37773@users.noreply.github.com>
2024-06-05 14:57:49 +0800

    Update DemoApplication.java

--- gradle/src/main/java/io/paketo/demo/DemoApplication.java
+++ gradle/src/main/java/io/paketo/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(proxyBeanMethods=false)
+@SpringBootApplication(proxyBeanMethods = false)
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
